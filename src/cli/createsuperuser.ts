9e65a8067a3a465ea69dfa365968ee9a6779665e
Amogh Madan <amoghmadaan@gmail.com>
2024-01-15 21:24:47 +0530

    Lint: Fix line length Interface: Use CreateUser for validatedData

--- src/cli/createsuperuser.ts
+++ src/cli/createsuperuser.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import { read } from 'read';
 
-import { User } from '../interfaces/user';
+import { CreateUser, User } from '../interfaces/user';
 import { UserModel } from '../models';
 import { accountsValidator } from '../validators';
 import { MONGODB_URI } from '../settings';
@@ -13,7 +13,11 @@ export default async function createSuperUser(): Promise<void> {
   const email: string = await read({ prompt: 'Email: ' });
   const firstName: string = await read({ prompt: 'First name: ' });
   const lastName: string = await read({ prompt: 'Last name: ' });
-  const password: string = await read({ prompt: 'Password: ', replace: '*', silent: true });
+  const password: string = await read({
+    prompt: 'Password: ',
+    replace: '*',
+    silent: true,
+  });
   const passwordAgain: string = await read({
     prompt: 'Password (again): ',
     replace: '*',
@@ -21,14 +25,15 @@ export default async function createSuperUser(): Promise<void> {
   });
 
   try {
-    const validatedData: User = await accountsValidator.createUser.validateAsync({
-      email,
-      firstName,
-      lastName,
-      password,
-      passwordAgain,
-      isAdmin: true,
-    });
+    const validatedData: CreateUser = await accountsValidator
+      .createUser
+      .validateAsync({
+        email,
+        firstName,
+        lastName,
+        password,
+        passwordAgain,
+      });
     await mongoose.connect(MONGODB_URI);
     const user: User | null = await UserModel.findOne({ email });
     if (user) {
@@ -36,8 +41,7 @@ export default async function createSuperUser(): Promise<void> {
       console.error('Error: That email is already taken.');
       process.exit(2);
     }
-    const newUser: User = new UserModel(validatedData);
-    await newUser.save();
+    await UserModel.create({ ...validatedData, isAdmin: true });
   } catch (err: unknown) {
     // eslint-disable-next-line no-console
     console.error(err);
