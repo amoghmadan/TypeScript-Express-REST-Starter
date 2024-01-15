e84160046c25fce893a9b7efd62d02e7ff9db478
Amogh Madan <amoghmadaan@gmail.com>
2024-01-15 21:25:29 +0530

    Fix imports and bring common content to const variables.

--- tests/accounts.test.ts
+++ tests/accounts.test.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
-import supertest, { Response } from 'supertest';
 import mongoose from 'mongoose';
+import supertest, { Response } from 'supertest';
 import TestAgent from 'supertest/lib/agent';
 
 import { getRequestListener } from '../src/cli/bootstrap';
@@ -9,8 +9,13 @@ import { UserModel } from '../src/models';
 import { MONGODB_URI } from '../src/settings';
 import { generateKey } from '../src/utilities/token';
 
+const ROOT_URL: string = '/api/accounts';
+const KEYWORD: string = 'Token';
 const EMAIL: string = 'test.user@email.com';
-const PASSWORD: string = 'foobarbaz';
+const CREDENTIALS: Record<string, string> = {
+  email: EMAIL,
+  password: 'foobarbaz',
+};
 
 const request: TestAgent = supertest(getRequestListener());
 
@@ -18,12 +23,9 @@ describe('Account API Tests', (): void => {
   beforeAll(async (): Promise<void> => {
     await mongoose.connect(MONGODB_URI);
     await UserModel.create({
-      email: EMAIL,
+      ...CREDENTIALS,
       firstName: 'Test',
       lastName: 'User',
-      password: PASSWORD,
-      isActive: true,
-      dateJoined: new Date(),
     });
   });
 
@@ -32,16 +34,17 @@ describe('Account API Tests', (): void => {
     await mongoose.connection.close();
   });
 
-  describe('POST /api/accounts/login', (): void => {
+  describe(`POST ${ROOT_URL}/login`, (): void => {
     it('Performs Account Login', async (): Promise<void> => {
-      const payload: { [key: string]: string } = { email: EMAIL, password: PASSWORD };
-      const response: Response = await request.post('/api/accounts/login').send(payload);
+      const response: Response = await request
+        .post(`${ROOT_URL}/login`)
+        .send(CREDENTIALS);
       expect(response.status).toBe(201);
       expect(response.body.token).toBeDefined();
     });
   });
 
-  describe('GET /api/accounts/detail', (): void => {
+  describe(`GET ${ROOT_URL}/detail`, (): void => {
     it('Retrieves Account Details', async (): Promise<void> => {
       const user: User | null = await UserModel.findOne({ email: EMAIL });
       if (!user) {
@@ -51,8 +54,8 @@ describe('Account API Tests', (): void => {
       await user.save();
 
       const response: Response = await request
-        .get('/api/accounts/detail')
-        .set('Authorization', `Token ${user.token.key}`);
+        .get(`${ROOT_URL}/detail`)
+        .set('Authorization', `${KEYWORD} ${user.token.key}`);
 
       expect(response.status).toBe(200);
       expect(response.body.email).toBe(user.email);
@@ -61,12 +64,12 @@ describe('Account API Tests', (): void => {
     });
 
     it('Returns 401 if not authorized', async (): Promise<void> => {
-      const response: Response = await request.get('/api/accounts/detail');
+      const response: Response = await request.get(`${ROOT_URL}/detail`);
       expect(response.status).toBe(401);
     });
   });
 
-  describe('DELETE /api/accounts/logout', (): void => {
+  describe(`DELETE ${ROOT_URL}/logout`, (): void => {
     it('Performs Account Logout', async (): Promise<void> => {
       const user: User | null = await UserModel.findOne({ email: EMAIL });
       if (!user) {
@@ -76,13 +79,13 @@ describe('Account API Tests', (): void => {
       await user.save();
 
       const response: Response = await request
-        .delete('/api/accounts/logout')
-        .set('Authorization', `Token ${user.token.key}`);
+        .delete(`${ROOT_URL}/logout`)
+        .set('Authorization', `${KEYWORD} ${user.token.key}`);
       expect(response.status).toBe(204);
     });
 
     it('Returns 401 if not authorized', async (): Promise<void> => {
-      const response: Response = await request.delete('/api/accounts/logout');
+      const response: Response = await request.delete(`${ROOT_URL}/logout`);
       expect(response.status).toBe(401);
     });
   });
