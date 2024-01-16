c33c4fbf046a88cf84ac8f1a6de38f22ff92fbd8
Amogh Madan <amoghmadaan@gmail.com>
2024-01-16 21:43:55 +0530

    Add more tests for better coverage.

--- tests/accounts.test.ts
+++ tests/accounts.test.ts
@@ -11,7 +11,7 @@ import { generateKey } from '../src/utilities/token';
 
 const ROOT_URL: string = '/api/accounts';
 const KEYWORD: string = 'Token';
-const EMAIL: string = 'test.user@email.com';
+const EMAIL: string = 'account.testuser@email.com';
 const CREDENTIALS: Record<string, string> = {
   email: EMAIL,
   password: 'foobarbaz',
@@ -24,14 +24,32 @@ describe('Account API Tests', (): void => {
     await mongoose.connect(MONGODB_URI);
     await UserModel.create({
       ...CREDENTIALS,
-      firstName: 'Test',
-      lastName: 'User',
+      firstName: 'Account',
+      lastName: 'Test User',
     });
   });
 
   afterAll(async (): Promise<void> => {
     await UserModel.deleteOne({ email: EMAIL });
     await mongoose.connection.close();
+  });
+
+  describe(`POST ${ROOT_URL}/login`, (): void => {
+    it('Incomplete Payload', async (): Promise<void> => {
+      const response: Response = await request
+        .post(`${ROOT_URL}/login`)
+        .send({ email: EMAIL });
+      expect(response.status).toBe(400);
+    });
+  });
+
+  describe(`POST ${ROOT_URL}/login`, (): void => {
+    it('Invalid Credentials', async (): Promise<void> => {
+      const response: Response = await request
+        .post(`${ROOT_URL}/login`)
+        .send({ email: EMAIL, password: 'foobarba' });
+      expect(response.status).toBe(401);
+    });
   });
 
   describe(`POST ${ROOT_URL}/login`, (): void => {
@@ -67,6 +85,27 @@ describe('Account API Tests', (): void => {
       const response: Response = await request.get(`${ROOT_URL}/detail`);
       expect(response.status).toBe(401);
     });
+
+    it('Returns 401 if Bearer instead of Token', async (): Promise<void> => {
+      const response: Response = await request
+        .get(`${ROOT_URL}/detail`)
+        .set('Authorization', 'Bearer Token');
+      expect(response.status).toBe(401);
+    });
+
+    it('Returns 401 if more than two value', async (): Promise<void> => {
+      const response: Response = await request
+        .get(`${ROOT_URL}/detail`)
+        .set('Authorization', 'This Bearer Token');
+      expect(response.status).toBe(401);
+    });
+
+    it('Returns 403 if invalid token', async (): Promise<void> => {
+      const response: Response = await request
+        .get(`${ROOT_URL}/detail`)
+        .set('Authorization', 'Token 0X1X2X3X4X5X6X7X8X');
+      expect(response.status).toBe(403);
+    });
   });
 
   describe(`DELETE ${ROOT_URL}/logout`, (): void => {
@@ -82,11 +121,6 @@ describe('Account API Tests', (): void => {
         .delete(`${ROOT_URL}/logout`)
         .set('Authorization', `${KEYWORD} ${user.token.key}`);
       expect(response.status).toBe(204);
-    });
-
-    it('Returns 401 if not authorized', async (): Promise<void> => {
-      const response: Response = await request.delete(`${ROOT_URL}/logout`);
-      expect(response.status).toBe(401);
     });
   });
 });
