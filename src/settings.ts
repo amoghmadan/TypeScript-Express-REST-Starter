413663ae2714d353297398b1d2f56cec32565195
Amogh Madan <amoghmadaan@gmail.com>
2024-01-17 17:54:42 +0530

    Drop dotenv and use --env-file flag.

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,5 @@
 import path from 'path';
 
-import 'dotenv/config';
-
 export const BASE_DIR: string = path.dirname(__filename);
 
 export const MONGODB_URI: string = String(process.env.MONGODB_URI);
